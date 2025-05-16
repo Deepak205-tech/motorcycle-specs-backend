e546f79993412a31776abfc6b4cfbb92e029416e
MeghDeep <meghdeep@MeghDeeps-MacBook-Pro.local>
2025-05-16 14:41:36 +0530

    fix cors

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-
+import cors from 'cors';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
